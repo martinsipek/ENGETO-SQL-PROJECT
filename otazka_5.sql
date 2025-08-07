0188dd2aa2a99a901e02bde762af0a12f54fb265
Martin Šípek <martinsipek@icloud.com>
2025-08-07 15:21:56 +0200

    Úprava dotazu dle zpětné vazby od lektora.
    Získávání vstupních dat z vytvořených tabulek.

--- otazka_5.sql
+++ otazka_5.sql
@@ -2,107 +2,76 @@
 -- 5) Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, 
 -- projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
 
-WITH gdp_cz AS ( -- CTE pro získání dat z ČR
+WITH gdp_cz AS ( -- výběr dat z t_martin_sipek_project_SQL_secondary_final
     SELECT
-        e.country,
-        e.year,
-        e.gdp
-    FROM economies e
-    WHERE 
-    	e.country = 'Czech Republic'
+        mss.year, 
+        mss.gdp 
+    FROM t_martin_sipek_project_SQL_secondary_final mss
+    WHERE country = 'Czech Republic' 
 ),
-gdp_changes AS ( -- CTE pro výpočet meziročních změn HDP (GDP) + připočtení jednoho roku dopředu pro zjištění vlivu HDP (GDP) v dalším roce
+gdp_changes AS ( -- výpočet meziročních změn HDP + posunutí o 1 rok dopředu (pro navazující srovnání s mzdami a cenami)
     SELECT
-        gcz.year + 1 AS shifted_year,
-        gcz.gdp,
-        LAG(gcz.gdp) OVER (ORDER BY gcz.year) AS previous_gdp,
-        ROUND((
-            CASE
-                WHEN LAG(gcz.gdp) OVER (
-                	ORDER BY 
-                		gcz.year) 
-                	IS NULL THEN NULL
-                ELSE 100.0 * (gcz.gdp - LAG(gcz.gdp) OVER (
-                	ORDER BY 
-                		gcz.year)) 
-                	/ LAG(gcz.gdp) OVER (
-                	ORDER BY 
-                		gcz.year)
-            END)::NUMERIC, 2) AS gdp_growth_percent
-    FROM gdp_cz gcz
+        year + 1 AS shifted_year, -- posun o 1 rok dopředu
+        gdp, 
+        LAG(gdp) OVER (ORDER BY year) AS previous_gdp, 
+        CASE 
+            WHEN LAG(gdp) OVER (ORDER BY year) IS NULL THEN NULL 
+            ELSE ROUND( -- výpočet změny v procentech
+                ((gdp - LAG(gdp) OVER (ORDER BY year)) / LAG(gdp) OVER (ORDER BY year))::numeric * 100, 2
+            )
+        END AS gdp_growth_percent -- meziroční změna HDP v procentech
+    FROM gdp_cz
 ),
-wages_year AS ( -- CTE pro výpočet průměrné mzdy v ČR podle let
+wages_year AS ( -- výpočet průměrné mzdy podle jednotlivých let
     SELECT
-        cp.payroll_year AS year,
-        ROUND(AVG(cp.value)::NUMERIC, 2) AS average_wage
-    FROM czechia_payroll cp
-    WHERE 
-    	cp.value_type_code = 5958
-    GROUP BY 
-    	cp.payroll_year
+        msp.year,
+        ROUND(AVG(average_monthly_wage)::numeric, 2) AS average_wage 
+    FROM t_martin_sipek_project_SQL_primary_final msp
+    GROUP BY year
 ),
-wage_changes AS ( -- CTE pro výpočet meziročního růstu mezd
+wage_changes AS ( -- výpočet meziročního růstu mezd
     SELECT
-        wy.year,
-        wy.average_wage,
-        LAG(wy.average_wage) OVER (ORDER BY wy.year) AS previous_wage,
-        ROUND((
-            CASE 
-                WHEN LAG(wy.average_wage) OVER (
-                	ORDER BY 
-                		wy.year) 
-                	IS NULL THEN NULL
-                ELSE 100.0 * (wy.average_wage - LAG(wy.average_wage) OVER (
-                	ORDER BY 
-                		wy.year)) 
-                	/ LAG(wy.average_wage) OVER (
-                	ORDER BY 
-                		wy.year)
-            END)::NUMERIC, 2) AS wage_growth_percent
-    FROM wages_year wy
+        year,
+        average_wage,
+        LAG(average_wage) OVER (ORDER BY year) AS previous_wage,
+        CASE 
+            WHEN LAG(average_wage) OVER (ORDER BY year) IS NULL THEN NULL
+            ELSE ROUND(
+                ((average_wage - LAG(average_wage) OVER (ORDER BY year)) / LAG(average_wage) OVER (ORDER BY year))::numeric * 100, 2
+            )
+        END AS wage_growth_percent -- meziroční změna mezd v procentech
+    FROM wages_year
 ),
-prices_year AS ( -- CTE pro výpočet průměrné cen potravin podle let
+prices_year AS ( -- výpočet průměrné ceny potravin podle jednotlivých let
     SELECT
-        EXTRACT(YEAR FROM cp.date_from) AS year,
-        ROUND(AVG(cp.value)::NUMERIC, 2) AS average_price
-    FROM czechia_price cp
-    GROUP BY 
-    	year
+        msp.year,
+        ROUND(AVG(average_price)::numeric, 2) AS average_price 
+    FROM t_martin_sipek_project_SQL_primary_final msp
+    GROUP BY year
 ),
-price_changes AS ( -- CTE pro výpočet meziroční změny cen potravin
+price_changes AS ( -- výpočet meziročního růstu cen potravin
     SELECT
-        py.year,
-        py.average_price,
-        LAG(py.average_price) OVER (
-        	ORDER BY 
-        		py.year) AS previous_price,
-        ROUND((
-            CASE 
-                WHEN LAG(py.average_price) OVER (
-                	ORDER BY 
-                		py.year) 
-                	IS NULL THEN NULL
-                ELSE 100.0 * (py.average_price - LAG(py.average_price) OVER (
-                	ORDER BY 
-                		py.year)) 
-                	/ LAG(py.average_price) OVER (
-                	ORDER BY 
-                		py.year)
-            END
-        )::NUMERIC, 2) AS price_growth_percent
-    FROM prices_year py
+        year,
+        average_price,
+        LAG(average_price) OVER (ORDER BY year) AS previous_price,
+        CASE 
+            WHEN LAG(average_price) OVER (ORDER BY year) IS NULL THEN NULL
+            ELSE ROUND(
+                ((average_price - LAG(average_price) OVER (ORDER BY year)) / LAG(average_price) OVER (ORDER BY year))::numeric * 100, 2
+            )
+        END AS price_growth_percent -- meziroční změna cen potravin v procentech
+    FROM prices_year
 ),
-combined_growth AS ( -- kompinace dat HDP (z předchozího roku), mzdy a ceny potravin (ze stejného roku)
+combined_growth AS ( -- spojení všech výpočtů dohromady
     SELECT
-        w.year,
-        g.gdp_growth_percent,
-        w.wage_growth_percent,
-        p.price_growth_percent
+        w.year, 
+        g.gdp_growth_percent, -- meziroční růst HDP 
+        w.wage_growth_percent, -- meziroční růst mezd
+        p.price_growth_percent -- meziroční růst cen potravin
     FROM gdp_changes g
-    JOIN wage_changes w ON g.shifted_year = w.year
+    JOIN wage_changes w ON g.shifted_year = w.year 
     JOIN price_changes p ON g.shifted_year = p.year
 )
 SELECT *
-FROM combined_growth cgr
-ORDER BY 
-	year;
+FROM combined_growth
+ORDER BY year; 
