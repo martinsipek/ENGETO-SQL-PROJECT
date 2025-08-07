f9920e4a22340600d2421396457737c99b9fe28c
Martin Šípek <martinsipek@icloud.com>
2025-08-07 15:14:04 +0200

    Úprava dotazu dle zpětné vazby od lektora.
    Získávání vstupních dat z vytvořených tabulek.

--- otazka_3.sql
+++ otazka_3.sql
@@ -1,37 +1,31 @@
 -- Finální SQL skript pro odpověď na výzkumnou otázku:
 -- 3) Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? 
 
-WITH prices_year AS ( -- CTE pro výpočet průměrné ceny potravin podle roku
+WITH prices_year AS ( -- výpočet průměrné roční ceny potravin
     SELECT
-        EXTRACT(YEAR FROM cp.date_from) AS year,
-        cp.category_code,
-        cpc.name AS food_type,
-        ROUND(AVG(cp.value)::NUMERIC, 2) AS average_price
-    FROM czechia_price cp
-    JOIN czechia_price_category cpc ON cp.category_code = cpc.code
+        msp.year,
+        msp.category_code, 
+        ROUND(AVG(average_price)::NUMERIC, 2) AS average_price -- průměrná roční cena
+    FROM t_martin_sipek_project_SQL_primary_final msp
     GROUP BY 
-    	year, 
-    	cp.category_code, 
-    	cpc.name
+        year, 
+        category_code
 ),
-price_changes AS ( -- CTE pro výpočet meziročních změn cen u jednotlivých potravin
+price_changes AS ( -- výpočet meziroční změny cen u jednotlivých potravin
     SELECT
-        py.year,
-        py.category_code,
-        py.food_type,
-        py.average_price,
-        LAG(py.average_price) OVER ( -- cena potraviny pro předchozí rok
-            PARTITION BY 
-            	py.category_code -- pro každou potravinu samostatně
-            ORDER BY 
-            	py.year
+        py.year, 
+        py.category_code, 
+        py.average_price, 
+        LAG(py.average_price) OVER ( -- cena potraviny v předchozím roce
+            PARTITION BY py.category_code -- pro každou potravinu 
+            ORDER BY py.year
         ) AS previous_year_price,
-        ROUND( -- výpočet rozdílu v procentech meziročních změn (100 * (aktuální rok - předchozí rok) / předchozí rok)
+        ROUND( -- výpočet meziročních změn v procentech
             CASE 
                 WHEN LAG(py.average_price) OVER (
                     PARTITION BY py.category_code
                     ORDER BY py.year
-                ) IS NULL THEN NULL
+                ) IS NULL THEN NULL 
                 ELSE 100.0 * (py.average_price - LAG(py.average_price) OVER (
                     PARTITION BY py.category_code
                     ORDER BY py.year
@@ -40,16 +34,17 @@ price_changes AS ( -- CTE pro výpočet meziročních změn cen u jednotlivých 
                     ORDER BY py.year
                 )
             END, 2
-        ) AS price_changes_percent
+        ) AS price_changes_percent -- procentuální změna cen mezi jednotlivými roky
     FROM prices_year py
 )
-SELECT -- výpočet průměrného meziročního růstu ceny pro jednotlivé potraviny
-    category_code,
-    food_type,
-    ROUND(AVG(price_changes_percent)::NUMERIC, 2) AS average_annual_growth_percent
-FROM price_changes ps
+SELECT
+    pc.category_code, 
+    cpc.name AS food_type, 
+    ROUND(AVG(pc.price_changes_percent)::NUMERIC, 2) AS average_annual_growth_percent -- průměrný roční růst ceny
+FROM price_changes pc
+JOIN czechia_price_category cpc ON pc.category_code = cpc.code -- JOIN tabulky pro zobrazení názvů potravin
 GROUP BY 
-	ps.category_code, 
-	ps.food_type
+    pc.category_code, 
+    cpc.name
 ORDER BY 
-	average_annual_growth_percent ASC;
+    average_annual_growth_percent ASC; 
